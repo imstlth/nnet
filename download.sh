65b7126d8c5b8c4c8edc46672c76ad227498e241
imstlth <64700983+imstlth@users.noreply.github.com>
2023-05-07 17:13:31 +0200

    Create a dir before download

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+mkdir neural_network
+cd neural_network
 wget https://github.com/imstlth/nnet/releases/download/v0.2.1/nnet https://github.com/imstlth/nnet/raw/main/train-images.idx3-ubyte https://github.com/imstlth/nnet/raw/main/train-labels.idx1-ubyte https://github.com/imstlth/nnet/raw/main/encoded.nnet
 chmod u+x nnet
 echo pour lancer le programme, écrivez ./nnet -D \$PWD/ 784,183,42,10
